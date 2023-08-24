2dff70d10dbf48e1fb63e327767322711cf1590e
presmae <precious.marson@synacy.com>
2023-08-25 07:36:35 +0800

    fix commit conflict

--- src/test/groovy/com/synacy/leaveapplication/admin/AdminControllerSpec.groovy
+++ src/test/groovy/com/synacy/leaveapplication/admin/AdminControllerSpec.groovy
@@ -1,6 +1,6 @@
 package com.synacy.leaveapplication.admin
 
-<<<<<<< HEAD
+
 import com.synacy.leaveapplication.UserRole
 import com.synacy.leaveapplication.user.UserController
 import com.synacy.leaveapplication.user.UserResponse
@@ -51,40 +51,40 @@ class UserControllerSpec extends Specification {
         UserRole.ADMIN == adminUserResponseList.getBody()[0].getRole()
     }
 
-    private AdminService adminService
-    public AdminController adminController
-
-    void setup() {
-        this.adminService = Mock(AdminService)
-        this.adminController = new AdminController(this.adminService)
-    }
-
-    def "fetchAdminList should return a response entity with the list of admins"() {
-        given:
-        List<Admin> adminListExpected = [Mock(Admin), Mock(Admin)]
-        this.adminService.getAdminList() >> adminListExpected
-
-        when:
-        ResponseEntity<List<Admin>> adminListResponse= this.adminController.fetchAdminList()
-
-        then:
-        HttpStatus.OK == adminListResponse.getStatusCode()
-        adminListExpected == adminListResponse.getBody()
-    }
-
-    def "fetchAdminUserList should return a response entity with the correct adminUsers"() {
-        given:
-        List<Admin> adminListExpected = [Mock(Admin)]
-        this.adminService.getAdminList() >> adminListExpected
-
-        when:
-        ResponseEntity<List<AdminUserResponse>> adminUserResponseList = this.adminController.fetchAdminUserList()
-
-        then:
-        HttpStatus.OK == adminUserResponseList.getStatusCode()
-        adminListExpected[0].getId() == adminUserResponseList.getBody()[0].getId()
-        adminListExpected[0].getName() == adminUserResponseList.getBody()[0].getName()
-        UserRole.ADMIN == adminUserResponseList.getBody()[0].getRole()
-    }
+//    private AdminService adminService
+//    public AdminController adminController
+//
+//    void setup() {
+//        this.adminService = Mock(AdminService)
+//        this.adminController = new AdminController(this.adminService)
+//    }
+//
+//    def "fetchAdminList should return a response entity with the list of admins"() {
+//        given:
+//        List<Admin> adminListExpected = [Mock(Admin), Mock(Admin)]
+//        this.adminService.getAdminList() >> adminListExpected
+//
+//        when:
+//        ResponseEntity<List<Admin>> adminListResponse= this.adminController.fetchAdminList()
+//
+//        then:
+//        HttpStatus.OK == adminListResponse.getStatusCode()
+//        adminListExpected == adminListResponse.getBody()
+//    }
+//
+//    def "fetchAdminUserList should return a response entity with the correct adminUsers"() {
+//        given:
+//        List<Admin> adminListExpected = [Mock(Admin)]
+//        this.adminService.getAdminList() >> adminListExpected
+//
+//        when:
+//        ResponseEntity<List<AdminUserResponse>> adminUserResponseList = this.adminController.fetchAdminUserList()
+//
+//        then:
+//        HttpStatus.OK == adminUserResponseList.getStatusCode()
+//        adminListExpected[0].getId() == adminUserResponseList.getBody()[0].getId()
+//        adminListExpected[0].getName() == adminUserResponseList.getBody()[0].getName()
+//        UserRole.ADMIN == adminUserResponseList.getBody()[0].getRole()
+//    }
 
 }
--- src/test/groovy/com/synacy/leaveapplication/admin/AdminServiceSpec.groovy
+++ src/test/groovy/com/synacy/leaveapplication/admin/AdminServiceSpec.groovy
@@ -31,27 +31,27 @@ class AdminServiceSpec extends Specification {
         userListExpected == userListActual
     }
 
-    public List<Admin> adminList;
-    private AdminRepository adminRepository;
-    public AdminService adminService;
-
-    void setup() {
-        this.adminList = new ArrayList<>()
-        this.adminRepository = Mock(AdminRepository)
-        this.adminService = new AdminService(this.adminList, this.adminRepository)
-    }
-
-    def "getAdminList should return the list of admin users"() {
-        given:
-        List<Admin> adminListExpected = [Mock(Admin), Mock(Admin)]
-        this.adminRepository.findAll() >> adminListExpected
-
-
-        when:
-        List<Admin> adminListActual = this.adminService.getAdminList()
-
-        then:
-        adminListExpected == adminListActual
-    }
+//    public List<Admin> adminList;
+//    private AdminRepository adminRepository;
+//    public AdminService adminService;
+//
+//    void setup() {
+//        this.adminList = new ArrayList<>()
+//        this.adminRepository = Mock(AdminRepository)
+//        this.adminService = new AdminService(this.adminList, this.adminRepository)
+//    }
+//
+//    def "getAdminList should return the list of admin users"() {
+//        given:
+//        List<Admin> adminListExpected = [Mock(Admin), Mock(Admin)]
+//        this.adminRepository.findAll() >> adminListExpected
+//
+//
+//        when:
+//        List<Admin> adminListActual = this.adminService.getAdminList()
+//
+//        then:
+//        adminListExpected == adminListActual
+//    }
 
 }
