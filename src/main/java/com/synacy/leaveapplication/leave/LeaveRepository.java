203312b985c113354aebbe7c5f9d4dbc219024aa
junsaku0 <junsaku.yamada@synacy.com>
2023-08-24 17:01:03 +0800

    Add userId as column to leave

--- src/main/java/com/synacy/leaveapplication/leave/LeaveRepository.java
+++ src/main/java/com/synacy/leaveapplication/leave/LeaveRepository.java
@@ -4,11 +4,14 @@ import com.synacy.leaveapplication.UserRole;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LeaveRepository extends JpaRepository<Leave,Long> {
 
     Page<Leave> findAllByNameAndRole(String name, UserRole role, Pageable pageable);
+
+//    @Query("SELECT user.name, leave FROM Users as user JOIN Leave as leave ON user.name = leave.name WHERE user.headId = :headId")
 
 }
