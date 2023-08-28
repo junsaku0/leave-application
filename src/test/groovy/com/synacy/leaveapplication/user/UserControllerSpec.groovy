3481c33edd3b48f1dcec2fed0cabfc99189e2214
junsaku0 <junsaku.yamada@synacy.com>
2023-08-28 22:21:50 +0800

    Edit leave response attribute to include leave id

--- src/test/groovy/com/synacy/leaveapplication/user/UserControllerSpec.groovy
+++ src/test/groovy/com/synacy/leaveapplication/user/UserControllerSpec.groovy
@@ -1,8 +1,5 @@
 package com.synacy.leaveapplication.user
 
-import com.synacy.leaveapplication.UserRole
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import spock.lang.Specification
 
 class UserControllerSpec extends Specification {
@@ -15,18 +12,5 @@ class UserControllerSpec extends Specification {
         userController = new UserController(userService)
     }
 
-//    def "fetchAdminUserList should return a response entity with the correct adminUsers"() {
-//        given:
-//        List<Users> adminListExpected = [Mock(Users)]
-//        this.userService.getAdminList() >> adminListExpected
-//
-//        when:
-//        ResponseEntity<List<UserResponse>> userResponseList = this.userController.fetchAdminUserList()
-//
-//        then:
-//        HttpStatus.OK == userResponseList.getStatusCode()
-//        adminListExpected[0].getId() == userResponseList.getBody()[0].getId()
-//        adminListExpected[0].getName() == userResponseList.getBody()[0].getName()
-//        UserRole.ADMIN == userResponseList.getBody()[0].getRole()
-//    }
+
 }
--- src/test/groovy/com/synacy/leaveapplication/user/UserServiceSpec.groovy
+++ src/test/groovy/com/synacy/leaveapplication/user/UserServiceSpec.groovy
@@ -1,8 +1,11 @@
 package com.synacy.leaveapplication.user
 
+import com.synacy.leaveapplication.UserRole
 import com.synacy.leaveapplication.leave.LeaveRepository
 import com.synacy.leaveapplication.leave.LeaveService
 import spock.lang.Specification
+
+import java.time.LocalDate
 
 class UserServiceSpec extends Specification {
 
@@ -11,8 +14,39 @@ class UserServiceSpec extends Specification {
 
     void setup() {
         this.userRepository = Mock(UserRepository)
-        this.userService = new UserService(this.userRepository)
+        this.userService = new UserService([], this.userRepository)
     }
 
+    def "createUser should save new user to repository with correct details"() {
+        given:
+        String name = 'name'
+        UserRole role = UserRole.MANAGER
+        String head = 'superAdmin'
+        LocalDate hireDate = LocalDate.now()
+        int totalLeave = 10
 
+        UserDetails userDetails = Mock(UserDetails)
+        userDetails.getName() >> name
+        userDetails.getRole() >> role
+        userDetails.getHead() >> head
+        userDetails.getHireDate() >> hireDate
+        userDetails.getTotalLeave() >> totalLeave
+
+        Users userHead = Mock(Users)
+
+        userRepository.findAllByName(head) >> Optional.of(userHead)
+
+        when:
+        userService.createUser(userDetails)
+
+        then:
+        1 * userRepository.save(_) >> {Users users ->
+            assert name == users.getName()
+            assert role == users.getRole()
+            assert userHead.getId() == users.getHeadId()
+            assert hireDate == users.getHireDate()
+            assert totalLeave == users.getTotalLeaves()
+        }
+
+    }
 }
